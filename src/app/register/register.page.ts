3b585daaf05a1f1edf4282df138a7bfd29ea04b7
Armand <armand.deffrennes@student.yncrea.fr>
2022-01-17 17:31:19 +0100

    Register & Login Front

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Display} from '../shared/class/display';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  // data utilisés pour la connexion
+  public registerData = {
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
 
-  constructor() { }
+  constructor(public router: Router, public display: Display) {
+    let after = false;
+    // récupère l'email dans le lien
+    for (const i of this.router.url) {
+      if (after && i !== '=') {
+        this.registerData.email += i;
+      } else if (i === '?') {
+        after = true;
+      }
+    }
+   }
 
   ngOnInit() {
   }
 
+  checkPwd(){
+    if (this.registerData.password !== this.registerData.confirmPassword){
+      this.registerData.password = '';
+      this.registerData.confirmPassword = '';
+      //Display error message
+      this.display.display("Attention les mots de passe sont différents !");
+      return;
+    }
+    //If password is good
+    this.makeRegister();
+    this.display.display({code:"Inscription réussie !", color: "success"});
+  }
+
+  makeRegister(){
+    this.router.navigateByUrl('identification').then();
+  }
 }
