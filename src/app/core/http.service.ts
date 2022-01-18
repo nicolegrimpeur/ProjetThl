a7f8847cf6604bb3c81ab81f06ea4cc2ce039427
Nicolas Barrat <nicolas.barrat@student.junia.com>
2022-01-18 17:29:36 +0100

    Ajout lien api

--- src/app/core/http.service.ts
+++ src/app/core/http.service.ts
@@ -9,6 +9,14 @@ export class HttpService {
   // private base = 'https://nicob.ovh/';
   private base = 'http://localhost:5000/';
 
+  private baseUrl = this.base + 'api';
+
   constructor(private readonly http: HttpClient) {
   }
+
+  // getUserQr() {
+  //   const url = this.baseUrl
+  // }
+
+
 }
