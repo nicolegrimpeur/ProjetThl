0b93d7166662efc4e59059c50c492491adce6e96
augustin-m <60687546+augustin-m@users.noreply.github.com>
2022-01-19 18:56:44 +0100

    Modal Infos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -48,10 +48,6 @@ const routes: Routes = [
     loadChildren: () => import('./shared/modal/modal-share/modal-share.module').then( m => m.ModalSharePageModule)
   },
   {
-    path: 'modal-confidentiality',
-    loadChildren: () => import('./shared/modal/modal-confidentiality/modal-confidentiality.module').then( m => m.ModalConfidentialityPageModule)
-  },
-  {
     path: 'modal-about',
     loadChildren: () => import('./shared/modal/modal-about/modal-about.module').then( m => m.ModalAboutPageModule)
   },
